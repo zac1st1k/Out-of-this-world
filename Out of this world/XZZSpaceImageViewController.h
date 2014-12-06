8e495694a90dfc41645e363446e2c3c41c637054
zac1st1k <zacfirst@gmail.com>
2014-12-06 19:20:06 +1100

    + Second view + Scroll view

---
+++ Out of this world/XZZSpaceImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  XZZSpaceImageViewController.h
+//  Out of this world
+//
+//  Created by Zac on 6/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZSpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
