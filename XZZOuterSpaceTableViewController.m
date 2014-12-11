14eaacaa53f8779def840ad17e43fa4d3a8d4b71
zac1st1k <zacfirst@gmail.com>
2014-12-11 19:33:02 +1100

    + Bug fixes

--- XZZOuterSpaceTableViewController.m
+++ XZZOuterSpaceTableViewController.m
@@ -301,14 +301,12 @@
 }
 */
 
-/*
 // Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
 
 /*
 #pragma mark - Navigation
